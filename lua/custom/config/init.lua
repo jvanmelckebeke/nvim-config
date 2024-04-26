7827329ac934e1e9dfb8357c9b8263deff7740e0
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2024-04-26 16:25:31 +0200

    changes

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -2,3 +2,6 @@
 vim.api.nvim_set_keymap('n', '<S-Tab>', '<<', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<S-Tab>', '<Esc><<i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
+
+-- space + f does telescope live_grep
+vim.api.nvim_set_keymap('n', '<Space>f', ':Telescope live_grep<CR>', { noremap = true, silent = true })
--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -5,6 +5,10 @@ return {
       require('auto-session').setup {
         log_level = 'error',
         auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+        auto_clean_after_session_restore = false,
+        pre_save_cmds = {
+          'Neotree close',
+        },
       }
     end,
   },
