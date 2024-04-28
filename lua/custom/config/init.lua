eb0fb86c2800b3b4840ed9a4f5b4cf041a0ae077
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2024-04-28 20:21:04 +0200

    docs for shift tab

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -1,4 +1,5 @@
 -- Set up key mappings in Normal, Insert, and Visual modes
+-- when shift tab is pressed, indent left
 vim.api.nvim_set_keymap('n', '<S-Tab>', '<<', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<S-Tab>', '<Esc><<i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
